4360d7f04483abf2f98897c5b6104990871a827f
Satheesh Kannan Chandra Bose <satheesh.chandra@yahoo.com>
2020-10-17 16:22:10 +0800

    Helper/wrapper program
    Adding Helper/wrapper program and dynamic segment code.

---
+++ modules/create_segment_in_emarsys_v2.py
@@ -0,0 +1,105 @@
+'''
+This program creates customer segment programatically in Emarsys Email Marketing platform.
+Dependencies: Adyen transaction files.
+Valid Emarsys API access
+''''
+
+import bootconfig as config
+import logging
+from adyentransactions import *
+from emarsys_api_py_v3 import * 
+from argparse import ArgumentParser
+
+df_packages = pd.DataFrame()
+df_payments = pd.DataFrame()
+processDate = ''
+
+def main():
+	global df_packages
+	df_packages = config.df_packages
+	
+	parser = ArgumentParser()
+	group = parser.add_mutually_exclusive_group()  # -f and -d options are mutually exclusive
+	group.add_argument('-f', action="store", dest="fileName")
+	group.add_argument('-d', action="store", dest="processDate", default=(date.today() - timedelta(1)).strftime('%Y_%m_%d')) 
+	args = parser.parse_args()
+
+	# input_file = args.fileName
+	global processDate
+	processDate = args.processDate  
+	logFile = ''.join([config.log_dir,os.path.basename(__file__).split('.')[0],'-',processDate,'.log'])
+	# print(logFile)
+	logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG, filename=logFile)
+	logging.info("Emarsys segment creation started for the date %s.", processDate)
+	df_list = []
+	for merchantName in config.adyenMerchants:
+		csvFile = 'received_payments_report_'+ processDate +'.csv'
+		targetLocation = config.data_dir + merchantName + '/'
+		retCode = getAdyenReport(csvFile, targetLocation, merchantName)
+		if retCode: logging.info("Adyen transaction file downloaded for %s as %s.", merchantName, retCode)
+		input_file = retCode if retCode else None
+		if input_file:
+			df = getPurchasesAll(input_file, config.packageFile)
+			df_list.append(df)
+	global df_payments
+	df_payments = pd.concat(df_list, axis=0, ignore_index=True)
+	config.df_payments = df_payments
+	logging.info('\n %s' ,  df_payments.groupby(by='PackageName').size().sort_values(ascending=False))
+	
+	for key, value in config.emarsysConfig["segmentPatterns"].items():
+		# package_pattern = value
+		# df_payments_package = getPurchases(df_payments, package_pattern)
+		logging.info("Emarsys segment creation started for the %s subscription.", key)
+		df_payments_package = getPurchases(df_payments, key)
+
+		segmentName = "Segment - {} Subscribers - Daily EDM".format(key)
+		contactListName = "Contacts - {} Subscribers - Daily EDM".format(key)
+		consolidatedList = "Contacts - {} Subscribers - Consolidated".format(key)
+		consolidatedSegment = "Segment - {} Subscribers - Consolidated".format(key)
+		
+		contactListID = getContactListId(contactListName, config.emarsysConfig['custCareAcct'])
+		segmentId = getSegmentId(segmentName, config.emarsysConfig['custCareAcct']) 
+		
+		if all([contactListID, segmentId]):
+			logging.info("Overwritting contacts in Emarsys for %s - Daily segment.", key)
+			df_payments_package.columns = ["3", "34348", "35020", "31"]
+			df_payments_package.drop('35020', axis=1, inplace=True)
+			retCode = createContactsFromDataFrame(df_payments_package, config.emarsysConfig['custCareAcct'])
+			totEmails = sum([len(i) for i in [json.loads(item)['data']['ids'] for item in retCode]])
+			logging.info("%s contacts updated in Emarsys for %s - Daily segment.", totEmails, key)
+			# retCode = overwriteContactListFromDataFrame(contactListName, df_payments_package, config.emarsysConfig['custCareAcct'])
+			retCode = addContactsListFromDataFrame(contactListName, df_payments_package, config.emarsysConfig['custCareAcct'])
+		else:
+			retCode = deleteSegment(segmentId, config.emarsysConfig['custCareAcct'])
+			retCode = deleteContactList(contactListID, config.emarsysConfig['custCareAcct'])
+			df_payments_package.columns = ["3", "34348", "35020", "31"]
+			df_payments_package.drop('35020', axis=1, inplace=True)
+			retCode = createContactsFromDataFrame(df_payments_package, config.emarsysConfig['custCareAcct'])
+			totEmails = sum([len(i) for i in [json.loads(item)['data']['ids'] for item in retCode]])
+			logging.info("%s contacts updated in Emarsys for %s - Daily segment.", totEmails, key)
+			retCode = createContactsListFromDataFrame(contactListName, df_payments_package, config.emarsysConfig['custCareAcct'])
+			contactListID = json.loads(retCode)["data"]["id"]
+			retCode = createSegment(segmentName, contactListID, config.emarsysConfig['custCareAcct'])
+		
+		df_payments_package.columns = ["3", "37470", "31"]
+		df_payments_package.drop('31', axis=1, inplace=True)
+		retCode = createContactsFromDataFrame(df_payments_package, config.emarsysConfig['tglAcct'])
+		totEmails = sum([len(i) for i in [json.loads(item)['data']['ids'] for item in retCode]])
+		logging.info("%s contacts updated in Emarsys for %s - Consolidated segment.", totEmails, key)
+		retCode = addContactsListFromDataFrame(consolidatedList, df_payments_package, config.emarsysConfig['tglAcct'])
+		contactListID = getContactListId(consolidatedList, config.emarsysConfig['tglAcct'])
+		retCode = createSegment(consolidatedSegment, contactListID, config.emarsysConfig['tglAcct'])
+		logging.info("Emarsys segment creation completed for the %s subscription.", key)
+	
+	logging.info("Emarsys segment creation Completed for the date %s.", processDate)
+	logging.info("Creating Pivot table based on L30 transactions.")
+	import adyen_purchase_pivot as adypvt
+	xlFile = adypvt.execute(processDate)
+	logging.info("Sending emailer to recepients.")
+	import send_mail
+	send_mail.send_email(df_payments, xlFile, processDate)
+	logging.info("END OF PROGRAM")
+
+if __name__ == "__main__": main()
+
+
---
+++ modules/emarsys_api_py_v3.py
@@ -0,0 +1,275 @@
+'''
+This is a utility/helper program used along with Emarsys Email Marketing platform API.  
+It is used to create audience lists and segments programmatically.
+'''
+
+import csv, http.client, json
+from emarsysheader import getAuthHeader  # This library is for authenticating user in Emarsys platform.
+
+def checkEmailIDs(contacts,accountName):
+	# Get the segment id for a given segment name
+	api_path = '/api/v2/contact/checkids'
+	payload="{\"key_id\":\"3\",\"external_ids\":" + json.dumps([contact for contact in contacts]) + "}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("POST", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	jdata = json.loads(data)
+	conn.close()
+	return jdata
+
+
+def getSegmentId(segmentName,accountName):
+	# Get the segment id for a given segment name
+	api_path = '/api/v2/filter'
+	payload = "{}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("GET", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	jdata = json.loads(data)
+	conn.close()
+	return next((item["id"] for item in jdata["data"] if item["name"] == segmentName), None)
+
+
+def deleteSegment(segmentId,accountName):
+	# Delete the given segment 
+	api_path = '/api/v2/filter/'+str(segmentId)+'/delete'
+	payload = "{}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("GET", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	# jdata = json.loads(data)
+	conn.close()
+	# return json.loads(data)["replyText"]
+	return data
+
+
+def getContactListId(contactListName,accountName):
+	# Get the ContactList id for a given contact list
+	api_path = '/api/v2/contactlist'
+	payload = "{}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("GET", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	jdata = json.loads(data)
+	conn.close()
+	return next((item["id"] for item in jdata["data"] if item["name"] == contactListName), None)
+
+def getContactsFromList(contactListName,accountName):
+	# Get the ContactList id for a given contact list
+	contactListID = getContactListId(contactListName,accountName)
+	api_path = '/api/v2/contactlist/' + contactListID 
+	payload = "{}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("GET", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	jdata = json.loads(data)
+	conn.close()
+	return jdata['data']
+	
+def getEmailsFromList(contactListName,accountName):
+	# Get the ContactList id for a given contact list
+	contactListID = getContactListId(contactListName,accountName)
+	listContacts = getContactsFromList(contactListName,accountName)
+	offset = 0
+	limit = 500
+	setSuperEmails = set()
+	while offset < len(listContacts):
+		api_path = '/api/v2/contactlist/' + contactListID + '/contacts/data?limit='+str(limit)+'&offset='+str(offset)+'&fields=3'
+		payload = "{}"
+		headers = getAuthHeader(accountName)
+		conn = http.client.HTTPSConnection("api.emarsys.net")
+		conn.request("GET", api_path, payload, headers)
+		res = conn.getresponse()
+		data = res.read()
+		jdata = json.loads(data)
+		for _, data in jdata['data'].items():
+			setSuperEmails.add(data['fields']['3'])
+		
+		offset = offset + limit
+	conn.close()
+	return setSuperEmails
+
+def deleteContactList(contactListID,accountName):
+	# Delete the given segment 
+	api_path = '/api/v2/contactlist/'+str(contactListID)+'/deletelist'
+	payload = "{}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("POST", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	# jdata = json.loads(data)
+	conn.close()
+	# return json.loads(data)["replyText"]
+	return data
+
+
+def getContactsFromFile(fileName):
+	with open(fileName) as f:
+	   reader=csv.DictReader(f)
+	   return list(reader)
+	   
+# Payload for creating contacts from CSV file
+
+def createContactsFromFile(fileName,accountName):
+	# Creates contacts from the file
+	api_path = '/api/v2/contact/?create_if_not_exists=1'
+	jContacts = json.dumps(getContactsFromFile(fileName))
+	payload="{\"key_id\":\"3\",\"contacts\":" + jContacts + "}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("PUT", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	conn.close()
+	return data
+	
+def createContactsFromDataFrame_v0(dataFrame,accountName):
+	# Creates contacts from the file
+	api_path = '/api/v2/contact/?create_if_not_exists=1'
+	jContacts = dataFrame.to_json(orient='records')
+	payload="{\"key_id\":\"3\",\"contacts\":" + jContacts + "}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("PUT", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	conn.close()
+	return data
+
+def createContactsFromDataFrame(dataFrame,accountName):
+	# Creates contacts from a dataframe
+	api_path = '/api/v2/contact/?create_if_not_exists=1'
+	offset=0
+	data=[]
+	while offset < len(dataFrame):
+		jContacts = dataFrame[offset:offset+1000].to_json(orient='records')
+		offset = offset + 1000
+		payload="{\"key_id\":\"3\",\"contacts\":" + jContacts + "}"
+		headers = getAuthHeader(accountName)
+		conn = http.client.HTTPSConnection("api.emarsys.net")
+		conn.request("PUT", api_path, payload, headers)
+		res = conn.getresponse()
+		data.append(res.read())
+		conn.close()
+	return data
+
+def createContactsList(listName, fileName, accountName):
+	# Creates contacts from the file
+	api_path = '/api/v2/contactlist'
+	contacts=getContactsFromFile(fileName)
+	payload = "{\"key_id\":\"3\",\"name\":\"" + listName + "\","
+	payload = payload + "\"external_ids\":" + json.dumps([contact["3"] for contact in contacts]) + "}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("POST", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	conn.close()
+	return data
+	
+
+def createContactsListFromDataFrame(listName, dataFrame, accountName):
+	# Creates contacts from the file
+	api_path = '/api/v2/contactlist'
+	# contacts=getContactsFromFile(fileName)
+	contacts = list(dataFrame['3'])
+	payload = "{\"key_id\":\"3\",\"name\":\"" + listName + "\","
+	payload = payload + "\"external_ids\":" + json.dumps([contact for contact in contacts]) + "}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("POST", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	conn.close()
+	return data
+
+def overwriteContactListFromDataFrame(listName, dataFrame, accountName):
+	# Overwrite the contactList 
+	contactListID = getContactListId(listName, accountName)
+	api_path = '/api/v2/contactlist/'+str(contactListID)+'/replace'
+	# contacts=getContactsFromFile(fileName)
+	contacts = list(dataFrame['3'])
+	payload = "{\"key_id\":\"3\",\"name\":\"" + listName + "\","
+	payload = payload + "\"external_ids\":" + json.dumps([contact for contact in contacts]) + "}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("POST", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	conn.close()
+	return data
+
+def addContactsListFromDataFrame(listName, dataFrame, accountName):   #Contacts - HBO Purchasers - Consolidated
+	# Creates contacts from the file
+	contactListID = getContactListId(listName,accountName)
+	if contactListID == None:
+		data = createContactsListFromDataFrame(listName, dataFrame, accountName)
+		return data
+	api_path = '/api/v2/contactlist/'+str(contactListID)+'/add'
+	contacts = list(dataFrame['3'])    #Assuming the dataframe has column 3 which is a field_id for email.
+	payload = "{\"key_id\":\"3\",\"name\":\"" + listName + "\","
+	payload = payload + "\"external_ids\":" + json.dumps([contact for contact in contacts]) + "}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("POST", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	conn.close()
+	return data
+
+def createSegment_v0(segmentName, contactListID, accountName):
+	# Creates contacts from the file
+	api_path = '/api/v2/filter'
+	payload = "{\"name\":\"" + segmentName + "\", \"baseContactListId\":\"" + str(contactListID) + "\"}"
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("PUT", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	conn.close()
+	return data
+
+def createSegment(segmentName, contactListID, accountName):
+	# Creates contacts from the file
+	api_path = '/api/v2/filter'
+	payload = "{\"name\":\"" + segmentName + "\", \"baseContactListId\":\"" + str(contactListID) + "\","
+	payload = payload + """
+	  "contactCriteria": {
+		"type": "and",
+		"children": [
+		  {
+			"type": "criteria",
+			"field": "optin",
+			"operator": "equals",
+			"value": "true"
+		  }
+		]
+	  }
+	}
+	"""
+	headers = getAuthHeader(accountName)
+	conn = http.client.HTTPSConnection("api.emarsys.net")
+	conn.request("PUT", api_path, payload, headers)
+	res = conn.getresponse()
+	data = res.read()
+	conn.close()
+	return data
+
+# '{"replyCode":0,"replyText":"OK","data":{"id":"778264"}}'
+
+def main(): pass
+
+if __name__ == "__main__": main()
+
+
